e9aec40d57c647508135ac2c7778406621eb93d9
Neil <nbauers@users.noreply.github.com>
2019-05-19 20:09:12 +0100

    Update gpx_maker_help.php

--- gpx_maker_help.php
+++ gpx_maker_help.php
@@ -13,7 +13,7 @@
 <p>You'll need to know the approximate latitude and longitude of your home location.</p>
 <ul>
   <li>The GPX Maker launches with a two point walk heading north from the start point to the end point.</li>
-  <li>Drag the southern start point to the start of the walk. Drag the northern end point somewhere near the end of the walk.</li>
+  <li>Drag the   start point to the start of the walk. Drag the northern end point somewhere near the end of the walk.</li>
   <li>Half way along the line is a blob. Drag this blob to a point somewhere along the route of the walk.</li>
   <li>Keep dragging the points and half-way marks to successively improve the accuracy of your track, as you zoom in.</li>
   <li>Pay particular attention to junctions and turning points. Less precision is needed over long fairly straight walk legs.</li>
@@ -21,9 +21,9 @@
 This makes it more obvious on the GPS display.</li>
   <li>You can switch between the aerial and OS views to improve your line positioning.</li>
   <li>Finally click one of the save buttons.</li>
-  <li>"Save GPX Track" can be downloaded diretly to some GPS receivers like the Garmin eTrex models.<br>The file name will be something like "2019-04-17T06_14_29.910Z.t.gpx. This is the save date and time in GMT.</li>
-<!--   <li>GPX route can be downloaded diretly to some GPS receivers and can be uploaded to Ramblers GB.<br>The file name will be something like "2019-04-17T06_14_29.910Z.r.gpx. This is the save date and time in GMT.</li> -->
-  <li>"Save KML" can be downloaded diretly into GoogleEarth (Desktop) if you have installed this.<br>The file name will be something like "2019-04-17T06_14_29.910Z.kml. This is the save date and time in GMT.</li>
+  <li>"Save GPX Track" can be downloaded diretly to some GPS receivers like the Garmin eTrex models.<br>The file name will be something like "2019-04-17T06_14_29.910Z.t.gpx.</li>
+  <li>"Save GPX Route" can be downloaded diretly to some GPS receivers and can be uploaded to Ramblers GB.<br>The file name will be something like "2019-04-17T06_14_29.910Z.r.gpx.</li>
+  <li>"Save KML" can be downloaded diretly into GoogleEarth (Desktop) if you have installed this.<br>The file name will be something like "2019-04-17T06_14_29.910Z.kml.</li>
   <li>To save more than one file type, use the "Return to Map" button. Then save the other file type or types.</li>
   <li>The file name follows the pattern YYYY-MM-DD T HH_MM_SS.MSZ.xxx.<br>(Year_month_day T hours_minutes_seconds.milliseconds Z).<br>Z indicates GMT. You can rename the file after saving.</li>
   <li>You can open the files in a simple text editor like Notepad. Here you can rename tracks, routes and waymarks.<br>Don't use a word processor for for this.</li>
