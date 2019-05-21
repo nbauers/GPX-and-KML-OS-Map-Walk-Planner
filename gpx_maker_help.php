f6ed531f284836c424a3868b9809cbf98749e5c5
Neil <nbauers@users.noreply.github.com>
2019-05-21 09:36:29 +0100

    Update gpx_maker_help.php

--- gpx_maker_help.php
+++ gpx_maker_help.php
@@ -6,11 +6,13 @@
 </head>
 <body style="font-size:105%">
 <h1>Waveney Ramblers GPX and KML Drawing Tool</h1>
+<h2>Launch</h2>
 <p>Launch the <a style="text-decoration:none" href="/gpx_maker.php?lat=52.4&lon=1.5">GPX Maker</a> near 52,4, 1.5 or at co-ordinates near your home area.</p>
 <p>Make a web browser bookmark or shortcut to launch the GPX Maker. It should look like this ...</p>
 <p style="font-family:Courier New, Courier, monospace">&nbsp; &nbsp; <b>http://waveneyramblers.org.uk/gpx_maker.php?lat=52.4&lon=1.5 </b></p>
 <p>Alter the numbers next to "lat" and "lon" to centre the map near your home location. Waveney is near 52,4, 1.5.</p>
 <p>You'll need to know the approximate latitude and longitude of your home location.</p>
+<h2>Draw Track</h2>
 <ul>
   <li>The GPX Maker launches with a two point walk heading north from the start point to the end point.</li>
   <li>Drag the   start point to the start of the walk. Drag the northern end point somewhere near the end of the walk.</li>
@@ -22,14 +24,28 @@
   <li>It's possible to displace a line slightly to indicate which side of a hedge to walk or to exagerate a kink (eg. hedge gap).<br>
 This makes it more obvious on the GPS display.</li>
   <li>You can switch between the aerial and OS views to improve your line positioning.</li>
-  <li>Finally click one of the save buttons.</li>
-  <li>"Save GPX Track" can be downloaded diretly to some GPS receivers like the Garmin eTrex models.</li>
-  <li>"Save GPX Route" can be downloaded diretly to some GPS receivers and can be uploaded to Ramblers GB.</li>
+</ul>
+<h2>Save Track</h2>
+<ul>
+  <li>Click one of the save buttons.</li>
+  <li>"Save GPX Track" can be downloaded directly to some GPS receivers like the Garmin eTrex models.</li>
+  <li>"Save GPX Route" can be downloaded directly to some GPS receivers and can be uploaded to Ramblers GB.</li>
   <li>"Save KML" can be downloaded diretly into GoogleEarth (Desktop) if you have installed this.<br>Other KML aware applications exist.</li>
   <li>To save more than one file type, use the "Return to Map" button. Then save the other file type or types.</li>
-  <li>The file name follows the pattern YYYY-MM-DD T HH_MM_SS.MSZ.xxx.<br>(Year_month_day T hours_minutes_seconds.milliseconds Z).<br>Z indicates GMT. You can rename the file after saving.</li>
+  <li>The file name follows the pattern YYYY-MM-DD T HH-MM-SS.ext.<br>(Year-month-day T hours:minutes:seconds).<br>You can rename the file during or after the save.</li>
+</ul>
+<h2>Rename Waypoints or Tracks</h2>
+<ul>
   <li>You can open the files in a simple text editor like Notepad, TextEdit or Leafpad. Here you can rename tracks, routes and waymarks.<br>Don't use a word processor for for this.</li>
-  <li>Before saving, if you understand the file formats, you can hand-edit the generated gpx or kml data, for example to rename the path or waypoints.</li>
+  <li>Before saving, you can hand-edit the generated gpx or kml data, for example to rename the path or waypoints.</li>
+</ul>
+<h2>Track Import</h2>
+<ul>
+  <li>Open a GPX or KML file in any simple text editor like Notepad, TextEdit or Leafpaf.</li>
+  <li>Copy the entire text (try CTRL+A CTRL+C)</li>
+  <li>Click the "Import KML / GPX" button.</li>
+  <li>Replace the instruction text with your copied text. Click the editing area and paste. (try CTRL+A CTRL+V).</li>
+  <li>Click the "Import KML / GPX" button. If the import fails you might get a blank map.</li>
 </ul>
 <h2>Bugs and Features</h2>
 <p>Please report bugs or request features by emailing Neil at nbauers@btinternet.com</p>
@@ -38,10 +54,10 @@ This makes it more obvious on the GPS display.</li>
 <p>&copy; This application and the included <a style="text-decoration:none" href="http://www.nearby.org.uk/tests/GeoTools.html" target="_blank">geotools.js</a> are licenced under the <a style="text-decoration:none" href="http://www.gnu.org/copyleft/gpl.html" target="_blank">GNU General Public Licence.</a></p>
 <h2>Disclaimer</h2>
 <ul>
-<li>This application is provided 'as is' with no guarantees.</li>
-<li>Routes prepared with this application can be used for navigation and uploaded to a handheld GPS Unit.</li>
-<li>Please verify the routes are correct (including accurately transferred to a GPS unit) before using them for Navigation.</li>
-<li>It's your responsibility, as nearly as possible, to plan legal routes using rights of way.</li>
+  <li>This application is provided 'as is' with no guarantees.</li>
+  <li>Routes prepared with this application can be used for navigation and uploaded to a handheld GPS Unit.</li>
+  <li>Please verify the routes are correct (including accurately transferred to a GPS unit) before using them for Navigation.</li>
+  <li>It's your responsibility, as nearly as possible, to plan legal routes using rights of way.</li>
 </ul>
 <h2>Github</h2>
 <p><a style="text-decoration:none" href="https://github.com/nbauers/GPX-and-KML-OS-Map-Walk-Planner" target="_blank">View on Github</a></p>
