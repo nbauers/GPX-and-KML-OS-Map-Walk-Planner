23468b6e57fbcbbd670ee7de140c169c2c310c4a
Neil <nbauers@users.noreply.github.com>
2019-05-21 08:01:58 +0100

    Update gpx_maker_help.php

--- gpx_maker_help.php
+++ gpx_maker_help.php
@@ -16,21 +16,24 @@
   <li>Drag the   start point to the start of the walk. Drag the northern end point somewhere near the end of the walk.</li>
   <li>Half way along the line is a blob. Drag this blob to a point somewhere along the route of the walk.</li>
   <li>Keep dragging the points and half-way marks to successively improve the accuracy of your track, as you zoom in.</li>
+  <li>Right clicking a point deletes it. If you accidentally delete too much, just press the F5 key to restart the tool.</li>
+  <li>Beware of the F5 key. This refreshes the web page and will erase your track.</li>
   <li>Pay particular attention to junctions and turning points. Less precision is needed over long fairly straight walk legs.</li>
   <li>It's possible to displace a line slightly to indicate which side of a hedge to walk or to exagerate a kink (eg. hedge gap).<br>
 This makes it more obvious on the GPS display.</li>
   <li>You can switch between the aerial and OS views to improve your line positioning.</li>
   <li>Finally click one of the save buttons.</li>
-  <li>"Save GPX Track" can be downloaded diretly to some GPS receivers like the Garmin eTrex models.<br>The file name will be something like "2019-04-17T06_14_29.910Z.t.gpx.</li>
-  <li>"Save GPX Route" can be downloaded diretly to some GPS receivers and can be uploaded to Ramblers GB.<br>The file name will be something like "2019-04-17T06_14_29.910Z.r.gpx.</li>
-  <li>"Save KML" can be downloaded diretly into GoogleEarth (Desktop) if you have installed this.<br>The file name will be something like "2019-04-17T06_14_29.910Z.kml.</li>
+  <li>"Save GPX Track" can be downloaded diretly to some GPS receivers like the Garmin eTrex models.</li>
+  <li>"Save GPX Route" can be downloaded diretly to some GPS receivers and can be uploaded to Ramblers GB.</li>
+  <li>"Save KML" can be downloaded diretly into GoogleEarth (Desktop) if you have installed this.<br>Other KML aware applications exist.</li>
   <li>To save more than one file type, use the "Return to Map" button. Then save the other file type or types.</li>
   <li>The file name follows the pattern YYYY-MM-DD T HH_MM_SS.MSZ.xxx.<br>(Year_month_day T hours_minutes_seconds.milliseconds Z).<br>Z indicates GMT. You can rename the file after saving.</li>
-  <li>You can open the files in a simple text editor like Notepad. Here you can rename tracks, routes and waymarks.<br>Don't use a word processor for for this.</li>
+  <li>You can open the files in a simple text editor like Notepad, TextEdit or Leafpad. Here you can rename tracks, routes and waymarks.<br>Don't use a word processor for for this.</li>
   <li>Before saving, if you understand the file formats, you can hand-edit the generated gpx or kml data, for example to rename the path or waypoints.</li>
 </ul>
 <h2>Bugs and Features</h2>
 <p>Please report bugs or request features by emailing Neil at nbauers@btinternet.com</p>
+<p>Outside the UK, grid references are not available so latitudes and longitudes are shown instead.</p>
 <h2>Copyright</h2>
 <p>&copy; This application and the included <a style="text-decoration:none" href="http://www.nearby.org.uk/tests/GeoTools.html" target="_blank">geotools.js</a> are licenced under the <a style="text-decoration:none" href="http://www.gnu.org/copyleft/gpl.html" target="_blank">GNU General Public Licence.</a></p>
 <h2>Disclaimer</h2>
